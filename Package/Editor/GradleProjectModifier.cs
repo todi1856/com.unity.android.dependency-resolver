e2ee0350c473a84adbda8136212ecffd192289ce
Tomas Dirvanauskas <tomas@unity3d.com>
2024-02-03 20:05:04 +0200

    WIP

---
+++ Package/Editor/GradleProjectModifier.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Unity.Android.Gradle;
+using UnityEditor.Android;
+
+
+namespace Unity.Android.DependencyResolver
+{
+    public class GradleProjectModifier : AndroidProjectFilesModifier
+    {
+        [Serializable]
+        public class Data
+        {
+            public string[] Repositories;
+            public string[] Dependencies;
+        }
+
+        public override AndroidProjectFilesModifierContext Setup()
+        {
+            var collector = new Collector();
+            var info = collector.CollectDependencies();
+
+            var context = new AndroidProjectFilesModifierContext();
+            context.SetData(nameof(Data), new Data()
+            {
+                Repositories = info.Repositories.Select(r => r.Value).ToArray(),
+                Dependencies = info.Dependencies.Select(d => d.Value).ToArray()
+            });
+            return context;
+        }
+
+        public override void OnModifyAndroidProjectFiles(AndroidProjectFiles projectFiles)
+        {
+            var data = projectFiles.GetData<Data>(nameof(Data));
+            foreach (var dependency in data.Dependencies)
+                projectFiles.UnityLibraryBuildGradle.Dependencies.AddDependencyImplementationByName(dependency);
+
+            var gradleRepositories = projectFiles.GradleSettings.DependencyResolutionManagement.Repositories;
+            foreach (var repository in data.Repositories)
+            {
+                var repo = gradleRepositories.AddRepository(Repositories.Maven);
+                var child = new Element($"url \"{repository}\"");
+                repo.AddElement(child);
+                // TALK TO RYTIS
+            }
+            // TODO: mavenLocal ?
+
+        }
+    }
+}
+
+// TODO: Add warning if specific templates are enabled
